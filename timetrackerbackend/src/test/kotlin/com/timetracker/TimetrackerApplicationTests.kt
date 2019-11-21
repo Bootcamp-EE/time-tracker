e5f63e2204dd3a4a1f4e24796b9018e42ff32a70
Swapnil Pande <swapnilpande@Swapnils-MacBook-Pro.local>
2019-11-21 19:51:39 +0530

    Adding kotlin-springboot
    Signed-off-by: Swapnil Pande <swapnilpande@Swapnils-MacBook-Pro.local>

---
+++ timetrackerbackend/src/test/kotlin/com/timetracker/TimetrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timetracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimetrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
