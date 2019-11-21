3c75c4ae1b3d9e426462fe792e324099c3a764d9
Imran Sayyed <isayyed@equalexperts.com>
2019-11-21 19:42:41 +0530

    Add terraform code to the repo.

---
+++ terraform/security_group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "http_security_group" {
+  name        = "allow_http"
+  description = "Allow all inbound http traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
