5fabe8fed83c7f110c68cc566584df9fc45f5242
sagarmate <smate@equalexperts.com>
2019-11-21 19:18:28 +0530

    added jenkins file

---
+++ ci/Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+
+    stages {
+        stage('Clone') {
+            git url: 'https://github.com/abhiShirbhate/time-tracker'
+        }
+
+        stage('Unit test') {
+            steps{
+                sh ''
+            }
+        }
+
+        stage('build') {
+            sh ''
+        }
+
+        stage('deploy') {
+
+        }
+
+    }
+}
