f4f1bbb469f59c38ab07a8230b5774dcc74a45ed
mlovewanshi <mohanlovewanshi@Mohans-MacBook-Pro.local>
2019-11-23 16:28:12 +0530

    Hello world unit test for React.

--- timetrackerfrontend/src/App.test.js
+++ timetrackerfrontend/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  configure({ adapter: new Adapter() });
+
+  it('app should renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('should render hello world', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('div').text();
+    expect(text).toEqual('Hello, World!');
+  });
+
 });
