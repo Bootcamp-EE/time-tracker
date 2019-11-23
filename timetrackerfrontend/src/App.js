ff2f6e2fc7c200603fb0101aacc791343f8854df
Drushti Ved <drushtived@Drushtis-MacBook-Pro.local>
2019-11-23 16:52:28 +0530

    Wired Backend API call in React to print mesage

--- timetrackerfrontend/src/App.js
+++ timetrackerfrontend/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      Hello, World!
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        greeting: ""
+    }
+    getGreeting = async () => {
+        let res = await axios.get("http://localhost:8080/");
+        this.setState({ greeting: res.data });
+    };
+    componentDidMount(){
+        this.getGreeting()
+    }
+    render(){
+        return (
+            <div className="App">
+                {this.state.greeting}
+            </div>
+        );
+    }
 }
 
 export default App;
--- timetrackerfrontend/src/apis/service.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "http://localhost:8080"
-export const getData = async (serviceUrl) => {  //    /hello
-    let data = await fetch(serviceUrl, {
-        method: 'GET'
-    }).then(response =>  response).then(posts => posts)
-
-    return data;
-}
