e6f8c48260419885fd7a256b74d67775fa42410b
Pramod Jangam <pjangam@equalexperts.com>
2019-11-26 15:17:46 +0530

    changes in test case to mock api call Removed unused svg file

--- timetrackerfrontend/src/App.test.js
+++ timetrackerfrontend/src/App.test.js
@@ -3,20 +3,44 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import mockAxios from "axios";
+import waitUntil from 'async-wait-until'
 
-describe('App', () => {
+
+describe('<App />', () => {
   configure({ adapter: new Adapter() });
+  const greetingText = 'Hello, World!'
 
-  it('app should renders without crashing', () => {
+
+  beforeEach(() => {
+  });
+
+  it('should renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  
-  it('should render hello world', () => {
-    const wrapper = shallow(<App />);
-    const text = wrapper.find('div').text();
-    expect(text).toEqual('Hello, World!');
-  });
 
+  it('should render hello world', async () => {
+    //given
+    var impl=mockAxios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: greetingText
+      })
+    );
+    //when
+    const wrapper = shallow(<App />);
+
+    //then
+    //assert method 2
+    setTimeout(() => {
+      const text = wrapper.find('div').text();
+      expect(text).toEqual(greetingText);
+    }, 100);
+
+     //assert method 1
+     await waitUntil(() => wrapper.state('greeting') != null);
+     const text = wrapper.find('div').text();
+     expect(text).toEqual(greetingText);
+  });
 });
