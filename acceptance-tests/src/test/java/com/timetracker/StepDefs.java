e8054368a7ff89468ce3e8a5fe3f9e24ef69e8a9
Christian Hujer <Christian.Hujer@nelkinda.com>
2019-12-11 19:54:23 +0530

    Make chrome driver selection code more extensible.

--- acceptance-tests/src/test/java/com/timetracker/StepDefs.java
+++ acceptance-tests/src/test/java/com/timetracker/StepDefs.java
@@ -7,24 +7,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
+import java.util.Map;
+
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+
 public class StepDefs {
+    private static final String CHROME_DRIVER_PATH = "src/test/resources/chrome-drivers/";
+    private static final Map<String, String> chromeDrivers = Map.of(
+            "Linux", "chromedriver_linux",
+            "Mac OS X", "chromedriver_mac",
+            "Windows", "chromedriver.exe" // TODO Verify map key on a Windows 10 machine
+    );
     public static WebDriver driver;
+
+    private static String getChromeDriver() {
+        return CHROME_DRIVER_PATH + chromeDrivers.getOrDefault(
+                getProperty("os.name"),
+                "OS Not Supported. Add chrome driver to " + CHROME_DRIVER_PATH + " and make an entry in chromeDrivers in this class."
+        );
+    }
 
     @Given("I open home page")
     public void open_home_page() {
-        System.setProperty("webdriver.chrome.driver", getChromeDriver());
+        setProperty("webdriver.chrome.driver", getChromeDriver());
         driver = new ChromeDriver();
         driver.get("http://localhost:3000");
-    }
-
-    private static String getChromeDriver() {
-        final String os = System.getProperty("os.name").toLowerCase();
-        if (os.startsWith("linux")) {
-            return "src/test/resources/chrome-drivers/chromedriver_linux";
-        } else if (os.startsWith("mac")) {
-            return "src/test/resources/chrome-drivers/chromedriver_mac";
-        }
-        throw new UnsupportedOperationException("Unsupported operating system: " + os);
     }
 
     @Then("I should see \"([^\"]*)\" message")
