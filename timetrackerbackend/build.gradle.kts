a9c62b49c75faa3688780cd5c81b42f889ac82ec
aashishghogre <aghogre@equalexperts.com>
2019-12-11 19:06:28 +0530

    update java version to 11

--- timetrackerbackend/build.gradle.kts
+++ timetrackerbackend/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -34,6 +34,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "11"
 	}
 }
