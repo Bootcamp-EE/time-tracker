2094d656a39ce952300ed1249149827c2df6a7c0
Christian Hujer <Christian.Hujer@nelkinda.com>
2019-12-11 19:34:53 +0530

    Add Linux support.

--- acceptance-tests/src/test/java/com/timetracker/StepDefs.java
+++ acceptance-tests/src/test/java/com/timetracker/StepDefs.java
@@ -11,14 +11,24 @@ public class StepDefs {
     public static WebDriver driver;
 
     @Given("I open home page")
-    public void open_home_page(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chrome-drivers/chromedriver_mac");
+    public void open_home_page() {
+        System.setProperty("webdriver.chrome.driver", getChromeDriver());
         driver = new ChromeDriver();
         driver.get("http://localhost:3000");
     }
 
+    private static String getChromeDriver() {
+        final String os = System.getProperty("os.name").toLowerCase();
+        if (os.startsWith("linux")) {
+            return "src/test/resources/chrome-drivers/chromedriver_linux";
+        } else if (os.startsWith("mac")) {
+            return "src/test/resources/chrome-drivers/chromedriver_mac";
+        }
+        throw new UnsupportedOperationException("Unsupported operating system: " + os);
+    }
+
     @Then("I should see \"([^\"]*)\" message")
-    public void iShouldSeeMessage(String message){
-        Assert.assertEquals(driver.findElement(By.className("App")).getText(),message);
+    public void iShouldSeeMessage(String message) {
+        Assert.assertEquals(driver.findElement(By.className("App")).getText(), message);
     }
 }
