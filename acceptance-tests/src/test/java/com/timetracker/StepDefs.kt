6d874000c8a178401d9ec680a28351782f748cd5
Christian Hujer <Christian.Hujer@nelkinda.com>
2019-12-18 19:14:46 +0530

    Improve Kotlin code, make it more idiomatic.

--- acceptance-tests/src/test/java/com/timetracker/StepDefs.kt
+++ acceptance-tests/src/test/java/com/timetracker/StepDefs.kt
@@ -1,33 +1,23 @@
 package com.timetracker
 
-import io.cucumber.java.Before
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.RemoteWebDriver
 
 class StepDefs {
-    @Before
-    fun initWebDriver() {
-        if (driver != null) return
-        System.setProperty("webdriver.chrome.driver", chromeDriver)
-        val options = ChromeOptions()
-        options.addArguments("--headless")
-        driver = ChromeDriver(options)
-        Runtime.getRuntime().addShutdownHook(Thread(Runnable { (driver as ChromeDriver).quit() }))
-    }
-
     @Given("I open the home page")
     fun open_home_page() {
-        driver!!["http://localhost:3000"]
+        driver["http://localhost:3000"]
     }
 
     @Then("I MUST see a message {string}")
     fun iMustSeeMessage(message: String?) {
-        Assertions.assertEquals(driver!!.findElement(By.className("App")).text, message)
+        assertEquals(driver.findElement(By.className("App")).text, message)
     }
 
     companion object {
@@ -37,7 +27,15 @@ class StepDefs {
                 "Mac OS X" to "chromedriver_mac",
                 "Windows" to "chromedriver.exe" // TODO Verify map key on a Windows 10 machine
         )
-        var driver: WebDriver? = null
+
+        val driver: WebDriver = run {
+            System.setProperty("webdriver.chrome.driver", chromeDriver)
+            val options = ChromeOptions()
+            options.addArguments("--headless")
+            Runtime.getRuntime().addShutdownHook(Thread(Runnable { (driver as RemoteWebDriver).quit() }))
+            ChromeDriver(options)
+        }
+
         private val chromeDriver: String
             get() = CHROME_DRIVER_PATH + chromeDrivers.getOrDefault(
                     System.getProperty("os.name"),
